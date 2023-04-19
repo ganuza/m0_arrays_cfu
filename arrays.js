026d49b136accf4d53fc6cae765406e21b8aaf2d
Adrian Zabolitzki <adrianzab@gmail.com>
2023-04-19 15:32:40 -0600

    add index positions explanation

--- arrays.js
+++ arrays.js
@@ -11,3 +11,8 @@ ages.shift();
 // the pop array method will remove the last element from the bool array
 bool.pop();
 
+// index positions relate to the order of the elements in an array
+// the positions start with position 0
+// in the array ["Tim", "Luke", "Mary"], "Tim" is in position 0, "Luke" is in pos 1, and "Mary" is in pos 2
+// these position numbers are helpful in defining which elements will be acted upon
+// for example:  calling slice(1, 3) will clip positions 1 and 2, but not 0 or 3

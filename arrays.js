7f8ed905fd3cde55513a5c2fb7f538891186ae42
Adrian Zabolitzki <adrianzab@gmail.com>
2023-04-19 15:23:45 -0600

    call array methods on each variable's array and provide explanation

--- arrays.js
+++ arrays.js
@@ -1,4 +1,13 @@
-var friends = ["Tim", "Sue", "Fran", "Bob"]
-var ages = [4, 7, 10, 12]
-var bool = [true, true, false, true]
+var friends = ["Tim", "Sue", "Fran", "Bob"];
+var ages = [4, 7, 10, 12];
+var bool = [true, true, false, true];
+
+// the unshift arrya method will add  the string "Primo" to the beginning of the friends array
+friends.unshift("Primo");
+
+// the shift array method will remove the first knumber from the ages array
+ages.shift();
+
+// the pop array method will remove the last element from the bool array
+bool.pop();
 
